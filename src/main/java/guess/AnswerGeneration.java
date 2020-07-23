f12094999ecb845c26ab5634303323758078cb6c
oliver-89 <wjr980729@163.com>
2020-07-23 21:59:07 +0800

    [GuessNumber] [oliver] bug-fixed: fixed ArrayIndexOutOfBoundsException

--- src/main/java/guess/AnswerGeneration.java
+++ src/main/java/guess/AnswerGeneration.java
@@ -6,7 +6,7 @@ public class AnswerGeneration implements AnswerGenerator{
     @Override
     public int[] generate() {
         int[] answer= new int[4];
-        for(int i =1;i<5;i++){
+        for(int i =0;i<4;i++){
             Random rand = new Random();
             int num = rand.nextInt(10);
             for(int asw:answer){
