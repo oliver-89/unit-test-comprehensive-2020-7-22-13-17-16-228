10b72fed51794ad232df131a647c5a6daa6e1cd8
oliver-89 <wjr980729@163.com>
2020-07-23 21:23:57 +0800

    [GuessNumber] [oliver] refactor: improve the details

--- src/main/java/guess/GuessNumber.java
+++ src/main/java/guess/GuessNumber.java
@@ -6,30 +6,37 @@ import java.util.Set;
 
 public class GuessNumber {
 
-    public static String guess(int[] input, int[] answer){
+    int choice = 0;
+
+    public String guess(int[] input, int[] answer) {
+
+        if (choice >= 6) {
+            return "You Have No Choice";
+        }
+        choice++;
 
         Set<Integer> tempSet = new HashSet<Integer>();
         for (int i = 0; i < input.length; i++) {
             tempSet.add(input[i]);
         }
-        if(input.length != tempSet.size()){
+        if (input.length != tempSet.size()) {
             return "Wrong Input，Input again";
         }
-        if(input.length!=4){
+        if (input.length != 4) {
             return "Wrong Input，Input again";
         }
 
         int num1 = 0;
         int num2 = 0;
-        if(Arrays.equals(input,answer)){
+        if (Arrays.equals(input, answer)) {
             return "4A0B";
         }
-        for(int i = 0;i<4;i++){
-            if(input[i] == answer[i]){
+        for (int i = 0; i < 4; i++) {
+            if (input[i] == answer[i]) {
                 num1++;
             }
-            for(int asw:answer){
-                if(asw==input[i]){
+            for (int asw : answer) {
+                if (asw == input[i]) {
                     num2++;
                 }
             }
@@ -49,7 +56,7 @@ public class GuessNumber {
 //        if(num1==0&&(num2-num1)==0){
 //            return "0A0B";
 //        }
-        return num1+"A"+(num2-num1)+"B";
+        return num1 + "A" + (num2 - num1) + "B";
     }
 
 }
