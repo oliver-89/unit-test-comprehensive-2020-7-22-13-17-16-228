0a5bf6f55c6c45ea3c5827a5edf1a6dffd4a8f4b
oliver-89 <wjr980729@163.com>
2020-07-23 19:22:05 +0800

    [GuessNumber] [oliver] feature:implement test case should_return_4A0B_when_guess_number_given_answer_1234_and_input_1234

--- src/main/java/guess/GuessNumber.java
+++ src/main/java/guess/GuessNumber.java
@@ -1,8 +1,13 @@
 package guess;
 
+import java.util.Arrays;
+
 public class GuessNumber {
 
     public String guess(int[] input,int[] answer){
+        if(Arrays.equals(input,answer)){
+            return "4A0B";
+        }
         return  null;
     }
 }
