9ba80f6574f63a0d48223265a25dbb49f07d8be1
oliver-89 <wjr980729@163.com>
2020-07-23 19:54:38 +0800

    [GuessNumber] [oliver] feature: implement should_return_2A2B_when_guess_number_given_answer_1234_and_input_1243

--- src/main/java/guess/GuessNumber.java
+++ src/main/java/guess/GuessNumber.java
@@ -4,10 +4,26 @@ import java.util.Arrays;
 
 public class GuessNumber {
 
-    public String guess(int[] input,int[] answer){
+    public static String guess(int[] input, int[] answer){
+        int num1 = 0;
+        int num2 = 0;
         if(Arrays.equals(input,answer)){
             return "4A0B";
         }
-        return  null;
+        for(int i = 0;i<4;i++){
+            if(input[i] == answer[i]){
+                num1++;
+            }
+            for(int asw:answer){
+                if(asw==input[i]){
+                    num2++;
+                }
+            }
+        }
+        if(num1==2&&(num2-num1)==2){
+            return "2A2B";
+        }
+        return null;
     }
+
 }
