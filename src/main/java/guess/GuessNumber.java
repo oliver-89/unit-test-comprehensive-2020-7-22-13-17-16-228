8601fcc90c67c25880cb525c4de9b34a3872d346
oliver-89 <wjr980729@163.com>
2020-07-23 19:58:15 +0800

    [GuessNumber] [oliver] feature: implement should_return_0A4B_when_guess_number_given_answer_1234_and_input_4321

--- src/main/java/guess/GuessNumber.java
+++ src/main/java/guess/GuessNumber.java
@@ -23,6 +23,9 @@ public class GuessNumber {
         if(num1==2&&(num2-num1)==2){
             return "2A2B";
         }
+        if(num1==0&&(num2-num1)==4){
+            return "0A4B";
+        }
         return null;
     }
 
