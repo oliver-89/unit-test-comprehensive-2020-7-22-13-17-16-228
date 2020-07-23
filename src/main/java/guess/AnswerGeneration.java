0f56979438efe425200994706f79446bb8a6ef4d
oliver-89 <wjr980729@163.com>
2020-07-23 21:30:03 +0800

    [GuessNumber] [oliver] feature:implement AnswerGeneration

---
+++ src/main/java/guess/AnswerGeneration.java
@@ -0,0 +1,22 @@
+package guess;
+
+import java.util.Random;
+
+public class AnswerGeneration implements AnswerGenerator{
+    @Override
+    public int[] generate() {
+        int[] answer= new int[4];
+        for(int i =1;i<5;i++){
+            Random rand = new Random();
+            int num = rand.nextInt(10);
+            for(int asw:answer){
+                if(asw==num){
+                    i--;
+                    continue;
+                }
+            }
+            answer[i] = num;
+        }
+        return answer;
+    }
+}
