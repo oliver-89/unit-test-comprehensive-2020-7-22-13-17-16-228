d86eb8d5fa5a62d82310ac176521ee4981b68867
oliver-89 <wjr980729@163.com>
2020-08-02 15:02:04 +0800

    [guess number] [oliver] refactor:Split function

--- src/main/java/guess/GuessNumber.java
+++ src/main/java/guess/GuessNumber.java
@@ -1,42 +1,83 @@
 package guess;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GuessNumber {
+    int times = 0;
+
+    public Boolean havaChance(){
+        if (times<6) {
+            times++;
+            return true;
+        }
+        return false;
+    }
+
+    public String printInput(int[] input){
+        StringBuilder str = new StringBuilder();
+        for(int num:input){
+            str.append(num).append(" ");
+        }
+        return str.toString();
+    }
+
+    public int[] getInput(){
+        int[] inputGuess = new int[4];
+        Scanner input = new Scanner(System.in);
+        for(int i = 0; i < 4; i++){
+            inputGuess[i] = input.nextInt();
+        }
+        return inputGuess;
+    }
+
+    public int[] getAnswer(AnswerGenerator answerGenerator){
+
+        return answerGenerator.generate();
+    }
 
 
     public String guess(int[] input, int[] answer) {
 
-        if (verifyInput(input)) {
-            int allcorrect = 0;
-            int correct = 0;
-
-            for (int i = 0; i < 4; i++) {
-                if (input[i] == answer[i]) {
-                    allcorrect++;
-                }
-                for (int asw : answer) {
-                    if (asw == input[i]) {
-                        correct++;
-                    }
-                }
-            }
-            return allcorrect + "A" + (correct - allcorrect) + "B";
+        if (isInputValid(input)) {
+            int allCorrect = allCorrect(input,answer);
+            int correct = correct(input,answer);
+            return format(allCorrect,correct);
         }
 
         return "Wrong Input，Input again";
+    }
 
+    public int allCorrect(int[] input, int[] answer){
+        int allCorrect =0;
+        for (int i =0;i<input.length;i++){
+            if(input[i]==answer[i]){
+                allCorrect++;
+            }
+        }
+        return allCorrect;
+    }
 
+    public int correct(int[] input, int[] answer){
+        int correct =0;
+        for (int num:input){
+            for(int asw:answer){
+                if (num==asw){
+                    correct++;
+                }
+            }
+        }
+        return correct;
+    }
+
+    public String format(int allCorrect,int correct){
+        return allCorrect+"A"+(correct - allCorrect) + "B";
     }
 
 
-    public boolean verifyInput(int[] input){
+    public boolean isInputValid(int[] input){
         return input.length == Arrays.stream(input).distinct().count()&&input.length == 4;
     }
 
--- src/main/java/guess/Start.java
+++ src/main/java/guess/Start.java
@@ -5,25 +5,35 @@ import java.util.Scanner;
 public class Start {
 
     public static void main(String[] args){
-        int times = 0;
+//        int times = 0;
+//        GuessNumber guessNumber = new GuessNumber();
+//        AnswerGenerator answerGenerator = new AnswerGeneration();
+//        int[] answer = answerGenerator.generate();
+//        int[] inputGuess = new int[4];
+//        Scanner input = new Scanner(System.in);
+//        while(times<6){
+//            for(int i = 0; i < 4; i++){
+//                inputGuess[i] = input.nextInt();
+//            }
+//            String str ="";
+//            for(int num:inputGuess){
+//                str+=num+" ";
+//            }
+//            times++;
+//            System.out.println(str+guessNumber.guess(inputGuess,answer));
+//        }
+//        input.close();
+//        System.out.println("You have no chances");
+//        System.out.println("You have no chances");
+
         GuessNumber guessNumber = new GuessNumber();
-        AnswerGenerator answerGenerator = new AnswerGeneration();
-        int[] answer = answerGenerator.generate();
-        int[] inputGuess = new int[4];
-        Scanner input = new Scanner(System.in);
-        while(times<6){
-            for(int i = 0; i < 4; i++){
-                inputGuess[i] = input.nextInt();
-            }
-            String str ="";
-            for(int num:inputGuess){
-                str+=num+" ";
-            }
-            times++;
-            System.out.println(str+guessNumber.guess(inputGuess,answer));
+        while (guessNumber.havaChance()){
+            int[] input =guessNumber.getInput();
+            int[] answer = guessNumber.getAnswer(new AnswerGeneration());
+            System.out.print(guessNumber.guess(input,answer)+" ");
+            System.out.println(guessNumber.printInput(input));
         }
-        input.close();
-        System.out.println("You have no chances");
+
         System.out.println("You have no chances");
 
 
